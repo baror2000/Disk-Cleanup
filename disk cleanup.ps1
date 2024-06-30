d7f6d74f2619212f5b3df0212b10a9acf8b6c563
baror2000 <84448326+baror2000@users.noreply.github.com>
2024-06-30 11:50:46 +0300

    Add files via upload

---
+++ disk cleanup.ps1
@@ -0,0 +1,53 @@
+﻿function Disk_Cleanup {
+    Show-ListDisk
+    Refresh_ListDisk
+    $disk_num = Read-Host "Please select your target disk number"
+    $letter = Read-Host "Please choose new drive letter"
+    $name = Read-Host "Please choose new drive name"
+
+    $diskpartScript = @"
+select disk $disk_num
+clean
+create partition primary
+active
+format fs=ntfs label="$name" quick
+assign letter=$letter
+"@
+
+    echo $diskpartScript | diskpart
+}
+
+function Show-ListDisk {
+    $listDiskScript = "list disk"
+    $output = echo $listDiskScript | diskpart | Out-String
+    Write-Output $output
+}
+
+function Refresh_ListDisk {
+    while ($true) {
+        $refresh = Read-Host "Please choose Refresh (R) or Continue (C)"
+        if ($refresh -eq "R") {
+            Show-ListDisk
+        } elseif ($refresh -eq "C") {
+            break
+        } else {
+            Write-Host "Incorrect option selected. Please choose Refresh (R) or Continue (C)."
+        }
+    }
+}
+
+function again {
+    while ($true) {
+        $again = Read-Host "Finished formatting the drive. Would you like to run again? (Y/N)"
+        if ($again -eq "Y") {
+            Disk_Cleanup
+        } elseif ($again -eq "N") {
+            exit
+        } else {
+            Write-Host "Incorrect option selected"
+        }
+    }
+}
+
+Disk_Cleanup
+again
